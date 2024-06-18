acda9b73a1e43131a1ef7d3c77cb2bec47af7257
Pedro Falcheti <pedrohenriquefalcheti@gmail.com>
2024-06-17 22:11:25 -0300

    .

--- lib/grade.dart
+++ lib/grade.dart
@@ -2,23 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_aula11_4/itens.dart';
 
 class Grade extends StatelessWidget {
-  const Grade({super.key});
+  const Grade({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Grade'),
+      ),
+      body: GridView.builder(
         itemCount: Itens.letras.length,
         gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (context, index) {
           return Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                height: 50,
-                width: 50,
-                color: Colors.blue,
-                child: Center(child: Text(Itens.letras[index])),
-              ));
-        });
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: 50,
+              width: 50,
+              color: Colors.blue,
+              child: Center(child: Text(Itens.letras[index])),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
